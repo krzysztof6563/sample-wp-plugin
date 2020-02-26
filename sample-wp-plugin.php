1e5c7193e877c4f5e3d53a9d3bc748433576f7e0
krzystof6563 <krzysztofmichalski42@gmail.com>
2020-02-26 08:43:33 +0100

    First commit

---
+++ sample-wp-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: XXXXX
+Description: XXXXX
+Author: BrodNet - Aplikacje Internetowe
+Version: 0.1
+Author URI: http://brodnet.pl/
+Text Domain: bn-xxxxx
+S3J6eXN6dG9mIE1pY2hhbHNraQ==
+*/
+
+if (! defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly
