dd6a9996d8648f5975f4a5b5c4aa3de808715a7b
krzystof6563 <krzysztofmichalski42@gmail.com>
2020-07-03 10:39:39 +0200

    Example of shortcode and post processing

--- admin.php
+++ admin.php
@@ -1,45 +1,45 @@
 <?php
-//change every bn_netto to bn_project
+//change every bn_project to bn_project
 
 $settingsArray = [
 	//General options
-	'bn_netto_section_settings' => [
-		'bn_netto_warning' => [
+	'bn_project_section_settings' => [
+		'bn_project_warning' => [
 			'label' => 'Tekst ostrzeżenia o cenach netto',
 			'type' => 'mce',
 		],
 	],
 ];
 
-function bn_netto_settings_init() {
+function bn_project_settings_init() {
 	global $settingsArray;
-	// register a new section in the "bn_netto" page
+	// register a new section in the "bn_project" page
 	add_settings_section(
-	'bn_netto_section_settings',
+	'bn_project_section_settings',
 	__( 'Opcje ogólne', 'bn-netto' ),
-	'bn_netto_section_settings_cb',
-	'bn_netto'
+	'bn_project_section_settings_cb',
+	'bn_project'
 	);	
 	//register invidual settings based on $settingsArray array 
 	foreach ($settingsArray as $setting_cat => $settings) {
 		foreach($settings as $setting => $data) {
-			register_setting('bn_netto', $setting);
+			register_setting('bn_project', $setting);
 			add_settings_field(
 				$setting, 
 				__( $data['label'], 'bn-netto' ),
-				'bn_netto_dispatch_options_cb',
-				'bn_netto',
+				'bn_project_dispatch_options_cb',
+				'bn_project',
 				$setting_cat, 
 				array_merge($data, ['name' => $setting])
 			);
 		}
 	}
 }
-add_action( 'admin_init', 'bn_netto_settings_init' );
+add_action( 'admin_init', 'bn_project_settings_init' );
 
-function bn_netto_section_settings_cb() {};
+function bn_project_section_settings_cb() {};
 
-function bn_netto_type_bool_cb($args) {
+function bn_project_type_bool_cb($args) {
 	$option = get_option( $args['name'] );
 	?>
 	<select name="<?= $args['name'] ?>">
@@ -49,7 +49,7 @@ function bn_netto_type_bool_cb($args) {
 	<?php
 }
 
-function bn_netto_type_text_cb($args) {
+function bn_project_type_text_cb($args) {
 	$option = get_option( $args['name'] );
 	?>
 	<input type="text" name="<?= $args['name'] ?>" value="<?= $option ?>">
@@ -58,7 +58,7 @@ function bn_netto_type_text_cb($args) {
 	<?php endif;
 }
 
-function bn_netto_type_color_cb($args) {
+function bn_project_type_color_cb($args) {
 	$option = get_option( $args['name'] );
 	?>
 	<input type="color" name="<?= $args['name'] ?>" value="<?= $option ?>">
@@ -67,7 +67,7 @@ function bn_netto_type_color_cb($args) {
 	<?php endif;
 }
 
-function bn_netto_type_font_cb($args) {
+function bn_project_type_font_cb($args) {
 	global $fonts;
 
 	$option = get_option( $args['name'] );
@@ -81,7 +81,7 @@ function bn_netto_type_font_cb($args) {
 		<p class="description">Należy ustawić opcję nadpisania kolorów powyżej, aby zadziałało.</p>
 	<?php endif;
 }
-function bn_netto_type_number_cb($args) {
+function bn_project_type_number_cb($args) {
 	$option = get_option( $args['name'] );
 	?>
 	<input type="number" name="<?= $args['name'] ?>" value="<?= $option ?>">
@@ -90,7 +90,7 @@ function bn_netto_type_number_cb($args) {
 	<?php endif;
 }
 
-function bn_netto_type_mce_cb($args) {
+function bn_project_type_mce_cb($args) {
 	$option = get_option( $args['name'] );
 	wp_editor($option, $args['name'], [
 		'text_area_name'=> $args['name'],//name you want for the textarea
@@ -99,14 +99,14 @@ function bn_netto_type_mce_cb($args) {
 	]);
 }
 
-function bn_netto_dispatch_options_cb($args) {
+function bn_project_dispatch_options_cb($args) {
 	if ($args['type'] == 'custom') {
 		if (function_exists($args['function'])) {
 			call_user_func($args['function'], $args);
 		}
 	} else {
-		if (function_exists('bn_netto_type_'.$args['type'].'_cb')) {
-			call_user_func('bn_netto_type_'.$args['type'].'_cb', $args);
+		if (function_exists('bn_project_type_'.$args['type'].'_cb')) {
+			call_user_func('bn_project_type_'.$args['type'].'_cb', $args);
 		}
 	}
 }
@@ -114,7 +114,7 @@ function bn_netto_dispatch_options_cb($args) {
 /**
  * Registers top level menu
  */
-function bn_netto_options_page() {
+function bn_project_options_page() {
     // add top level menu page
     if ( empty ( $GLOBALS['admin_page_hooks']['bn_options'] ) ) {
         add_menu_page(
@@ -131,18 +131,18 @@ function bn_netto_options_page() {
 		__('Komunikaty', 'bn-netto'),
 		__('Komunikaty', 'bn-netto'),
 		'manage_options',
-		'bn_netto_options',
-		'bn_netto_options_page_html'
+		'bn_project_options',
+		'bn_project_options_page_html'
 	);
 }
-add_action( 'admin_menu', 'bn_netto_options_page' );
+add_action( 'admin_menu', 'bn_project_options_page' );
 
 
 /**
 * top level menu:
 * callback functions
 */
-function bn_netto_options_page_html() {
+function bn_project_options_page_html() {
 	// check user capabilities
 	if (! current_user_can('manage_options')) {
 		return;
@@ -152,21 +152,21 @@ function bn_netto_options_page_html() {
 	// wordpress will add the "settings-updated" $_GET parameter to the url
 	if ( isset( $_GET['settings-updated'] ) ) {
 		// add settings saved message with the class of "updated"
-		add_settings_error( 'bn_netto_messages', 'bn_netto_message', __( 'Zapisano ustawienia', 'bn_netto' ), 'updated' );
+		add_settings_error( 'bn_project_messages', 'bn_project_message', __( 'Zapisano ustawienia', 'bn_project' ), 'updated' );
 	}
 
 	// show error/update messages
-	settings_errors( 'bn_netto_messages' );
+	settings_errors( 'bn_project_messages' );
 	?>
 	<div class="wrap">
 	<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 	<form action="options.php" method="post">
 	<?php
-	// output security fields for the registered setting "bn_netto"
-	settings_fields( 'bn_netto' );
+	// output security fields for the registered setting "bn_project"
+	settings_fields( 'bn_project' );
 	// output setting sections and their fields
-	// (sections are registered for "bn_netto", each field is registered to a specific section)
-	do_settings_sections( 'bn_netto' );
+	// (sections are registered for "bn_project", each field is registered to a specific section)
+	do_settings_sections( 'bn_project' );
 	// output save settings button
 	submit_button( 'Zapisz' );
 	?>
--- sample-wp-plugin.php
+++ sample-wp-plugin.php
@@ -5,7 +5,7 @@ Description: XXXXX
 Author: BrodNet - Aplikacje Internetowe
 Version: 0.1
 Author URI: http://brodnet.pl/
-Text Domain: bn-xxxxx
+Text Domain: bn-project
 S3J6eXN6dG9mIE1pY2hhbHNraQ==
 */
 
@@ -16,4 +16,29 @@ if (! defined('ABSPATH')) {
 
 include __DIR__."/admin.php";
 
-//ADD YOUR CODE HERE
+/**
+ * POST HANDLING EXAMPLE
+ * 
+ * Make sure that your form contains following code:
+ * <form action="<?php echo esc_url( admin_url('admin-post.php') ); ?>" method="post">
+ * <input type="hidden" name="action" value="contact_form">
+ * 
+ */
+function wp_post_example() {
+    //POST and GET are available here
+}
+add_action( 'admin_post_nopriv_contact_form', 'wp_post_example');
+add_action( 'admin_post_contact_form', 'wp_post_example' );
+
+/**
+ * SHORTCODE EXAMPLE
+ */
+function shortcode_example( $atts ) {
+	$a = shortcode_atts( array(
+		'foo' => 'something',
+		'bar' => 'something else',
+	), $atts );
+
+	return "SOME HTML";
+}
+add_shortcode( 'example', 'shortcode_example' );
